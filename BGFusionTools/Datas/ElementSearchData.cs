c2ef5b116e801d533b9674235802feddaa75fce0
Cheng <Ming.Cheng@beumer.com>
2019-09-02 13:28:11 +0800

    Revert "Merger V2.0&V3.0"
    This reverts commit 44c2d872f9dbcdfb854246787c62126dcfd7908f.

--- BGFusionTools/Datas/ElementSearchData.cs
+++ BGFusionTools/Datas/ElementSearchData.cs
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 ﻿using BGFusionTools.Helper;
 using BGFusionTools.Serialization;
 using System;
-=======
-﻿using System;
->>>>>>> parent of 24775a4... V2.03
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -17,12 +13,7 @@ using System.Xml.Serialization;
 
 namespace BGFusionTools.Datas
 {
-<<<<<<< HEAD
     public class ElementSearchData : BaseData
-=======
-    [XmlRoot("elements")]
-    public class ElementSearchData : BaseData,IXmlSerializable
->>>>>>> parent of 24775a4... V2.03
     {
         public ElementSearchData() { }
         public ElementSearchData(BaseParameter ConvParameter)
@@ -31,7 +22,6 @@ namespace BGFusionTools.Datas
         }
         public override List<T1> CreateList<T1>(CreateDataRow<T1, ConveyorRow> dataMath)
         {
-<<<<<<< HEAD
             return base.CreateList<T1>(dataMath);
         }
         public List<ElementSeacrhStruct> CreateElements(ConveyorRow conveyor)
@@ -42,102 +32,6 @@ namespace BGFusionTools.Datas
             return element;
         }
   
-=======
-            try
-            {
-                foreach (DataRow selectConRow in baseParameter.TaglistTable.Rows)
-                {
-
-                    if (selectConRow[baseParameter.TaglistColName[1, 0]].ToString() == "")
-                        break;
-                    string sSystem = selectConRow[baseParameter.TaglistColName[1, 0]].ToString();
-                    string sPlcLink = selectConRow[baseParameter.TaglistColName[1, 1]].ToString();
-                    string sEquipmentLine = selectConRow[baseParameter.TaglistColName[1, 3]].ToString();
-                    string sEquipmentElement = selectConRow[baseParameter.TaglistColName[1, 4]].ToString();
-                    string sElementName = string.Format("{0}_{1}_{2}_{3}", sSystem, sPlcLink, sEquipmentLine, sEquipmentElement);
-                    string sDisplayName = selectConRow[baseParameter.TaglistColName[1, 12]].ToString();
-                    string sLevel1View = selectConRow[baseParameter.TaglistColName[1, 15]].ToString();
-                    string sLevel2View = selectConRow[baseParameter.TaglistColName[1, 16]].ToString();
-                    string sDrawViews = selectConRow[baseParameter.TaglistColName[1, 17]].ToString();
-                    if (sDrawViews == "All" || sDrawViews == "Level2Only")
-                        elements.Add(new Element(sDisplayName, sLevel1View, sLevel2View, sElementName));
-                }
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show("数据处理错误" + e.Message);
-            }
-
-            return Functions.DataConvert.ToString(elements);
-        }
-        public override void OutData()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override DataTable ToDataTable()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Dictionary<string, string> ToDictionary()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int ToInt()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override List<List<string>> ToList()
-        {
-            throw new NotImplementedException();
-        }
-
-        public XmlSchema GetSchema()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ReadXml(XmlReader reader)
-        {
-            elements.Clear();
-            while (reader.Read())
-            {
-                if (reader.IsStartElement("element"))
-                {
-                    Element element = new Element();
-                    element.DisplayName = reader["displayname"];
-                    element.Level1 = reader["level1"];
-                    element.Level2 = reader["level2"];
-                    element.Name = reader["name"];
-                    elements.Add(element);
-                }
-            }
-        }
-
-        public void WriteXml(XmlWriter writer)
-        {
-            foreach(Element et in elements)
-            {
-                writer.WriteStartElement("element");
-                writer.WriteAttributeString("displayname", et.DisplayName);
-                writer.WriteAttributeString("level1", et.Level1);
-                writer.WriteAttributeString("level2", et.Level2);
-                writer.WriteAttributeString("name", et.Name);
-                writer.WriteEndElement();
-            }
-        }
-        public static ElementSearchData operator +(ElementSearchData a, ElementSearchData b)
-        {
-            ElementSearchData c = new ElementSearchData();
-            //b.elements.RemoveAt(0);
-            a.elements.AddRange(b.elements);
-            c.elements.AddRange(a.elements);
-            return c;
-        }
->>>>>>> parent of 24775a4... V2.03
     }
 
 
--- BGFusionTools/Datas/TestList.cs
+++ BGFusionTools/Datas/TestList.cs
@@ -10,12 +10,13 @@ using System.Collections;
 namespace BGFusionTools.Datas
 {
     class TestList:BaseData
-    { 
+    {
+        private Dictionary<string, string> dictionaryData;
+        private List<TestListStruct> listData=new List<TestListStruct>();
         public TestList(BaseParameter ConverParameter)
         {
             this.baseParameter = ConverParameter;
         }
-<<<<<<< HEAD
         public Dictionary<string, string> DictionaryData { get { return dictionaryData; } set { dictionaryData = value; } }
         public List<TestListStruct> ListData { get { return listData; } set { listData = value; } }
     }
@@ -29,86 +30,10 @@ namespace BGFusionTools.Datas
             _signalMappingTable = signalMappingTable;
             _signalColumn = signalColumn;
             _elementList = elementList;
-=======
-        public override Dictionary<string,string> ToDictionary()
-        {
-            //ListDatas = new List<ListData>();
-            Dictionary<string, string> lDictionary = new Dictionary<string, string>();
-            EnumerableRowCollection<DataRow> MainRows = LinqToTable();
-            try
-            {
-                switch (baseParameter.ViewNum)
-                {
-                    case 0:
-                        break;
-                    case 1:
-                        var ELementLineGroups = from p in MainRows
-                                                group p by new { system = p.Field<string>(baseParameter.TaglistColName[1, 0]), plc = p.Field<string>(baseParameter.TaglistColName[1, 1]), line = p.Field<string>(baseParameter.TaglistColName[1, 3]), view = p.Field<string>(baseParameter.TaglistColName[1, 16]) } into pp
-                                                select pp;
-                        foreach (var ELementLineGroup in ELementLineGroups)
-                        {
-                            string sSystem = ELementLineGroup.Key.system;
-                            string sPlcLink = ELementLineGroup.Key.plc;
-                            string sEquipmentLine = ELementLineGroup.Key.line;
-                            string sAreaLevel2view = ELementLineGroup.Key.view;
-                            
-                            string sLinesGroups = sPlcLink + "." + sEquipmentLine + "." + sAreaLevel2view;
-                            foreach (DataRow selectConRow in ELementLineGroup)
-                            {
-                                string sEquipmentElement = selectConRow[4].ToString();
-                                string sElementName = sEquipmentLine + "." + sEquipmentElement;
-                                lDictionary.Add(sElementName, sLinesGroups);
-                            }
-                        }
-                        break;
-                    case 2:
-
-                        foreach (DataRow selectConRow in MainRows)
-                        {
-                            //string sPoweBox = selectConRow[2].ToString();
-                            string sPlcLink = selectConRow[1].ToString();
-                            string sEquipmentLine = selectConRow[3].ToString();
-                            string sEquipmentElement = selectConRow[4].ToString();
-                            string sSingleMapping1 = selectConRow[6].ToString();
-                            string sAreaLevel2view = selectConRow[16].ToString();
-                            string sElementName = sEquipmentLine + "." + sEquipmentElement;
-                            lDictionary.Add(sElementName, sPlcLink);
-                        }
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("数据处理错误：" + ex.Message);
-            }
-            return lDictionary;
         }
 
-        public override string ToString()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int ToInt()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override List<List<string>> ToList()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override DataTable ToDataTable()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void OutData()
-        {
-            throw new NotImplementedException();
->>>>>>> parent of 24775a4... V2.03
-        }
-
+        public EnumerableRowCollection<DataRow> SignalMappingTable { get { return _signalMappingTable; } set { _signalMappingTable =value; } }
+        public List<string> ElementList { get { return _elementList; } set { _elementList = value; } }
+        public DataColumnCollection SignalColumn { get { return _signalColumn; } set { _signalColumn = value; } }
     }
 }
